daedf1f176c4b8e54b8512145dd1e0c3a42a8bf5
tvirus-01 <shakisa54@gmail.com>
2019-03-16 22:15:24 +0600

    menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,7 @@ const  menubtn = document.querySelector('.menu-btn');
 const  menu = document.querySelector('.menu');
 const  menubranding = document.querySelector('.menu-branding');
 const  menunav = document.querySelector('.menu-nav');
-const  menuitem = document.querySelectorAll('.menu-item');
+const  menuitem = document.querySelectorAll('.nav-item');
 
 let showmenu = false;
 
